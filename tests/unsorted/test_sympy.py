21d7c700182c580c9283092dda0253edf77f6830
Eric Berquist <eric.berquist@gmail.com>
2018-10-20 16:41:31 -0400

    symbolic SymPy example from Stack Overflow

---
+++ tests/unsorted/test_sympy.py
@@ -0,0 +1,26 @@
+# https://stackoverflow.com/questions/39558515/how-to-get-the-gradient-and-hessian-sympy
+
+from sympy import symbols
+from sympy import Symbol, Matrix, Function, simplify
+from sympy.tensor.array import derive_by_array
+
+
+eta, xi, sigma = symbols('eta xi sigma')
+
+x = Matrix([[xi], [eta]])
+
+h = [Function('h_' + str(i+1))(x[0], x[1]) for i in range(3)]
+z = [Symbol('z_' + str(i+1)) for i in range(3)]
+
+lamb = 0
+for i in range(3):
+    lamb += 1 / (2 * sigma ** 2) * (z[i] - h[i]) ** 2
+lamb = simplify(lamb)
+
+gradient = derive_by_array(lamb, (eta, xi))
+print(type(gradient))
+print(gradient)
+
+hessian = derive_by_array(gradient, (eta, xi))
+print(type(hessian))
+print(hessian)
