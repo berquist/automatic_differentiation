fea212f5ba5410274d8ff05433ff68ed7cba5524
Eric Berquist <eric.berquist@gmail.com>
2018-10-28 16:41:25 -0400

    stubs

--- dualmat.py
+++ dualmat.py
@@ -12,6 +12,7 @@ class DualMat2D:
     first: Scalar = attr.ib()
     second: Scalar = attr.ib()
     mat: np.ndarray = attr.ib(init=False)
+
     @mat.default
     def init_mat(self):
         _type = type(self.first)
@@ -25,3 +26,33 @@ class DualMat2D:
     @staticmethod
     def _lift(primitive: Scalar) -> 'DualMat2D':
         return DualMat2D(primitive, 0)
+
+    def __add__(self, other: Union['DualMat2D', Scalar]) -> 'DualMat2D':
+        if isinstance(other, Scalar):
+            return self + DualMat2D._lift(other)
+        else:
+            return self + other
+
+    def __radd__(self, other: Union['DualMat2D', Scalar]) -> 'DualMat2D':
+        return self + other
+
+    def __sub__(self, other: Union['DualMat2D', Scalar]) -> 'DualMat2D':
+        if isinstance(other, Scalar):
+            return self - DualMat2D._lift(other)
+        else:
+            return self - other
+
+    def __rsub__(self, other: Union['DualMat2D', Scalar]) -> 'DualMat2D':
+        return self - other
+
+    def __mul__(self, other: Union['DualMat2D', Scalar]) -> 'DualMat2D':
+        if isinstance(other, Scalar):
+            pass
+        else:
+            pass
+
+    def __rmul__(self, other: Union['DualMat2D', Scalar]) -> 'DualMat2D':
+        return self * other
+
+    def __truediv__(self, other: Union['DualMat2D', Scalar]) -> 'DualMat2D':
+        raise NotImplementedError
--- tests/test_dualmat.py
+++ tests/test_dualmat.py
@@ -6,6 +6,7 @@ from dualmat import DualMat2D
 def test_dualmat_lift():
     a = -3
     b = DualMat2D._lift(a)
+    # pylint: disable=E201
     c = np.array([[ -3,  0],
                   [  0, -3]], dtype=int)
     np.testing.assert_equal(b.mat, c)
@@ -14,7 +15,20 @@ def test_dualmat_lift():
 def test_dualmat_init():
     a = -3
     b = 1
+    # pylint: disable=E201
     c = np.array([[ -3,  1],
                   [  0, -3]], dtype=int)
     d = DualMat2D(a, b)
     np.testing.assert_equal(d.mat, c)
+
+
+def test_dualmat_add():
+    pass
+
+
+def test_dualmat_sub():
+    pass
+
+
+def test_dualmat_mul():
+    pass
