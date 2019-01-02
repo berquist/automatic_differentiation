625572580fc98f65e112b9dd89466f895a3da994
Eric Berquist <berquist@isi.edu>
2019-01-02 11:38:37 -0500

    Add examples from HIPS/autograd README

---
+++ tests/test_autograd.py
@@ -0,0 +1,17 @@
+import autograd.numpy as np
+from autograd import grad
+from autograd import elementwise_grad as egrad
+
+
+def test_readme():
+
+    def tanh(x):
+        y = np.exp(-2.0 * x)
+        return (1.0 - y) / (1.0 + y)
+
+    grad_tanh = grad(tanh)
+    assert grad_tanh(1.0) == 0.41997434161402603
+    assert (tanh(1.0001) - tanh(0.9999)) / 0.0002 == 0.41997434264973155
+
+    egrad_tanh = egrad(tanh)
+    assert egrad_tanh(1.0) == 0.41997434161402603
