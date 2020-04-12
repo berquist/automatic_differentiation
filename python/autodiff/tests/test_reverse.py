db779b9b75aa85147129a29c7f5fc44682a0c4d0
Eric Berquist <eric.berquist@gmail.com>
2020-04-12 16:05:27 -0400

    start comparison against pytorch and autograd

--- python/autodiff/tests/test_reverse.py
+++ python/autodiff/tests/test_reverse.py
@@ -1,4 +1,9 @@
-import numpy as np
+import math
+
+import autograd
+import autograd.numpy as np
+import torch
+from pytest import approx
 
 from autodiff.reverse import Var
 
@@ -16,3 +21,30 @@ def test_reverse_example() -> None:
     assert abs(z.value - 2.579425538604203) <= thresh
     assert (x.grad() - (y.value + np.cos(x.value))) <= thresh
     assert (y.grad() - x.value) <= thresh
+
+
+def test_sin() -> None:
+    """Make sure the derivative of the sine is consistent between
+    autodiff.reverse, pytorch, and autograd.
+    """
+    y = 4.2
+    z = math.sin(y)
+    ref = math.cos(y)
+
+    # autodiff.reverse
+    y_r = Var(y)
+    z_r = y_r.sin()
+    z_r.grad_value = 1.0
+
+    # pytorch
+    y_t = torch.tensor(y, dtype=torch.float64, requires_grad=True)
+    z_t = y_t.sin()
+    z_t.backward()
+
+    # autograd
+    def f(a):
+        return np.sin(a)
+
+    assert y_r.grad() == approx(ref)
+    assert y_t.grad.item() == approx(ref)
+    assert autograd.grad(f)(y) == approx(ref)
